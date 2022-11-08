182acc6a810a4ce6c35792bdbc193991f2579525
Nazar Sinilov <nsinilov.itrum@gmail.com>
2022-11-08 12:11:44 +0300

    changed updateOne func

--- backend/src/modules/controllers/task.controller.js
+++ backend/src/modules/controllers/task.controller.js
@@ -1,23 +1,24 @@
-const Search = require("../../db/modules/task/index")
+const Search = require("../../db")
 const fs = require("fs/promises")
 
 module.exports.getAllData = async (_, res) => {
+
     try {
         const allData = await fs.readFile("array.json", "utf-8")
         res.send({data: allData})
+
     } catch (err) {
         res.status(500).send({message: err.message})
     }
 }
 
 module.exports.createNewData = async (req, res) => {
+
     try {
         const {sortBy, sortDir, searchText, searchResult} = req.body
-        const item = await Search.updateOne({sortBy, sortDir, searchText} , {searchResult})
-        if (!item) {
-            await Search.create({sortBy, sortDir, searchText, searchResult})
-        }
+        await Search.updateOne({sortBy, sortDir, searchText} , {searchResult}, {upsert: true})
         res.send({data: {success: true}})
+
     } catch (err) {
         res.status(500).send({success: false})
     }
